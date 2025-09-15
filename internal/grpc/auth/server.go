8d85a9e4512a7ef03defcbd2e1c97230b7745d0f
Evgeniy Seleznev <evgen.seleznev@gmail.com>
2025-09-15 14:25:34 +0300

    added hadlers for service layer

--- internal/grpc/auth/server.go
+++ internal/grpc/auth/server.go
@@ -4,24 +4,115 @@ import (
 	"context"
 	auth_v1 "github.com/evgeniySeleznev/gRPC-auth/pkg/authV1"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
+
+//место использования интерфейса — интерфейс в сигнатуре функции
+//место реализации — где описывается соответствие интерфейсу, без использования
+
+//здесь содержится основная бизнес-логика
+//не в хендлерах ниже, а здесь в сервисном слое
+
+type Auth interface {
+	Get(ctx context.Context, email string, password string, appID int32) (token string, err error)
+	RegisterNewUser(ctx context.Context, email string, password string) (userID int64, err error)
+	IsAdmin(ctx context.Context, userID int64) (bool, error)
+}
 
 type serverApi struct {
 	auth_v1.UnimplementedAuthServer
+	auth Auth
 }
 
-func Register(gRPC *grpc.Server) {
-	auth_v1.RegisterAuthServer(gRPC, &serverApi{})
+func Register(gRPC *grpc.Server, auth Auth) {
+	auth_v1.RegisterAuthServer(gRPC, &serverApi{auth: auth})
 }
+
+const (
+	emptyValue = 0
+)
 
 func (s *serverApi) Get(ctx context.Context, req *auth_v1.GetRequest) (*auth_v1.GetResponse, error) {
-	return &auth_v1.GetResponse{Token: req.GetEmail()}, nil
+	if err := validateLogin(req); err != nil {
+		return nil, err
+	}
+
+	token, err := s.auth.Get(ctx, req.GetEmail(), req.GetPassword(), req.GetAppId())
+	if err != nil {
+		// TODO: обработка в зависимости от ошибки
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+
+	return &auth_v1.GetResponse{Token: token}, nil
 }
 
 func (s *serverApi) Create(ctx context.Context, req *auth_v1.CreateRequest) (*auth_v1.CreateResponse, error) {
-	panic("implement me")
+	if err := validateLRegister(req); err != nil {
+		return nil, err
+	}
+
+	userID, err := s.auth.RegisterNewUser(ctx, req.GetEmail(), req.GetPassword())
+	if err != nil {
+		// TODO: проверить ошибку пользователя — что не internal ошибка
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+
+	return &auth_v1.CreateResponse{
+		Id: userID,
+	}, nil
 }
 
 func (s *serverApi) IsAdmin(ctx context.Context, req *auth_v1.IsAdminRequest) (*auth_v1.IsAdminResponse, error) {
-	panic("implement me")
+	if err := validateIsAdmin(req); err != nil {
+		return nil, err
+	}
+
+	isAdmin, err := s.auth.IsAdmin(ctx, req.GetUserId())
+	if err != nil {
+
+		// TODO: проверить верно ли всё юзер ввёл
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+
+	return &auth_v1.IsAdminResponse{IsAdmin: isAdmin}, nil
+}
+
+// валидация логина
+func validateLogin(req *auth_v1.GetRequest) error {
+	if req.GetEmail() == "" {
+		return status.Error(codes.InvalidArgument, "email missing")
+	}
+
+	if req.GetPassword() == "" {
+		return status.Error(codes.InvalidArgument, "password is required")
+	}
+
+	if req.GetAppId() == emptyValue {
+		return status.Error(codes.InvalidArgument, "app_id is required")
+	}
+
+	return nil
+}
+
+// валидация регистрации
+func validateLRegister(req *auth_v1.CreateRequest) error {
+	if req.GetEmail() == "" {
+		return status.Error(codes.InvalidArgument, "email missing")
+	}
+
+	if req.GetPassword() == "" {
+		return status.Error(codes.InvalidArgument, "password is required")
+	}
+
+	return nil
+}
+
+// валидация проверки на админа
+func validateIsAdmin(req *auth_v1.IsAdminRequest) error {
+	if req.GetUserId() == emptyValue {
+		return status.Error(codes.InvalidArgument, "empty user id")
+	}
+
+	return nil
 }
