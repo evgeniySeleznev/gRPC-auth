a3b3a96ea5339e70cbd14daa426fea1ab6a94d01
Evgeniy Seleznev <evgen.seleznev@gmail.com>
2025-09-23 02:15:10 +0300

    started prepairing func tests

--- go.mod
+++ go.mod
@@ -15,12 +15,14 @@ require (
 
 require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/brianvoe/gofakeit/v6 v6.28.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-sqlite3 v1.14.32 // indirect
+	github.com/stretchr/testify v1.11.1 // indirect
 	golang.org/x/net v0.44.0 // indirect
 	golang.org/x/sys v0.36.0 // indirect
 	golang.org/x/text v0.29.0 // indirect
--- tests/suite/suite.go
+++ tests/suite/suite.go
@@ -4,25 +4,25 @@ import (
 	"context"
 	"github.com/evgeniySeleznev/gRPC-auth/internal/config"
 	auth_v1 "github.com/evgeniySeleznev/gRPC-auth/pkg/authV1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"net"
 	"os"
+	"strconv"
 	"testing"
 )
 
+const grpcHost = "localhost"
+
 type Suite struct {
 	*testing.T                    //потребуется для вызова методов *testing.T внутри Suite
-	cfg        *config.Config     //конфигурация приложения
+	Cfg        *config.Config     //конфигурация приложения
 	AuthClient auth_v1.AuthClient //Клиент для взаимодействия с gRPC-сервером
 }
 
 func New(t *testing.T) (context.Context, *Suite) {
 	t.Helper()
 	t.Parallel() // тестим параллельные обращения к сервису
-
-	//key := "CONFIG_PATH"
-	//if v := os.Getenv(key); v != "" {
-	//	return v
-	//} заготовка для получения конфига из переменной окружения
-	// не запускаем на гитхабе, поэтому норм
 
 	cfg := config.MustLoadByPath("../config/local_tests.yaml")
 
@@ -33,5 +33,30 @@ func New(t *testing.T) (context.Context, *Suite) {
 		cancelCtx()
 	})
 
-	//2:43:00
+	cc, err := grpc.DialContext(context.Background(),
+		grpcAddress(cfg),
+		grpc.WithTransportCredentials(insecure.NewCredentials())) // Используем insecure-коннект для тестов
+	if err != nil {
+		t.Fatalf("grpc server connection failed: %v", err)
+	}
+
+	return ctx, &Suite{
+		T:          t,
+		Cfg:        cfg,
+		AuthClient: auth_v1.NewAuthClient(cc),
+	}
+}
+
+func configPath() string {
+	const key = "CONFIG_PATH"
+
+	if v := os.Getenv(key); v != "" {
+		return v
+	}
+
+	return "../config/local_tests.yaml"
+}
+
+func grpcAddress(cfg *config.Config) string {
+	return net.JoinHostPort(grpcHost, strconv.Itoa(cfg.GRPC.Port))
 }
